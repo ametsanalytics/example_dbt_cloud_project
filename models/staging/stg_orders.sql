c086838318e5bb25c94d8b82a0350fdb63bed85a
Melissa Bottomley <ametsanalytics@gmail.com>
2024-10-22 12:09:30 +0000

    1. staging tables created using CodeGen package for efficiency : https://hub.getdbt.com/dbt-labs/codegen/latest/
    2. source columns renamed in staging layer to replace code in legacy model customer orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,20 @@
+-- generated using CodeGen
+with source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date as order_placed_at,
+        status as order_status
+
+    from source
+
+)
+
+select * from renamed
